8b99fdebeb78ebd822b24fa283703fe62226bb5e
USER <USER@MIN>
2024-09-03 18:15:36 +0900

    Board 롬복없어서 게터세터 추가함

--- Board/src/application/DTO/Board.java
+++ Board/src/application/DTO/Board.java
@@ -37,5 +37,70 @@ public class Board {
 		this.writer = writer;
 		this.content = content;
 	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Date getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+
+	public Date getUpdDate() {
+		return updDate;
+	}
+
+	public void setUpdDate(Date updDate) {
+		this.updDate = updDate;
+	}
+
+	public int getView() {
+		return view;
+	}
+
+	public void setView(int view) {
+		this.view = view;
+	}
+
+	@Override
+	public String toString() {
+		return "Board [no=" + no + ", title=" + title + ", writer=" + writer + ", content=" + content + ", regDate="
+				+ regDate + ", updDate=" + updDate + ", view=" + view + "]";
+	}
+	
+
+	
 	
 }
