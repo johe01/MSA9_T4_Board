fbee342f44eaf932d299a1cddf769885df242405
johe01 <jhe0598@gmail.com>
2024-09-04 18:25:28 +0900

    수정

---
+++ Board/src/application/controller/UpdateController.java
@@ -0,0 +1,35 @@
+package application.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class UpdateController {
+
+    @FXML
+    private TextArea content;
+
+    @FXML
+    private Text regDate;
+
+    @FXML
+    private TextField title;
+
+    @FXML
+    private Button updBtn;
+
+    @FXML
+    private Text updDate;
+
+    @FXML
+    private TextField writer;
+
+    @FXML
+    void updBoard(ActionEvent event) {
+
+    }
+
+}
